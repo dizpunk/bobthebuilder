c1129be8a25356337e15bb80c25baf30e6d4b2d2
Alessandro Girardini <102290380+lazyHatter@users.noreply.github.com>
2023-03-31 22:56:14 +0200

    Update Fedora.sh

--- fedora/Fedora.sh
+++ fedora/Fedora.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-set -e
+
+echo "---------- Installing Essential Packages ----------"
 
 # Cleanup and install essential packages
 sudo dnf check-update
 sudo dnf upgrade -y
 sudo dnf remove -y libreoffice-* rhythmbox
 sudo dnf install -y vim git gcc python3 btop speedtest-cli gnome-tweaks
+
+echo "---------- Essential Packages Installed ----------"
