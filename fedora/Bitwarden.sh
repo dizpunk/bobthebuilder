1780be172a21649f49105c01b699617fa007e762
Alessandro Girardini <102290380+lazyHatter@users.noreply.github.com>
2023-03-31 23:01:37 +0200

    Update Bitwarden.sh

--- fedora/Bitwarden.sh
+++ fedora/Bitwarden.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
-set -e
 
-# Get filename
-echo -n "Bitwarden executable: "
-read BITWARDEN
+echo "---------- Installing Bitwarden ----------"
 
-# Make executable
+# Get .AppImage executable
+curl -L "https://vault.bitwarden.com/download/?app=desktop&platform=linux" --output ./Bitwarden.AppImage
+BITWARDEN="$(pwd)/Bitwarden.AppImage"
+
+# Set permissions
 chmod 755 $BITWARDEN
 
 # Move to new folder
 mkdir $HOME/Apps
 mv $BITWARDEN $HOME/Apps
 
+# Install pairus icons
+sudo dnf install -y papirus-icon-theme
+
 # Create .desktop entry 
-cat << EOF | tee Bitwarden.desktop
+cat << EOF | tee Bitwarden.desktop &> /dev/null 
 [Desktop Entry]
 Type=Application
 Version=1.0
 Name=Bitwarden
 Icon=/usr/share/icons/Papirus/48x48/apps/bitwarden.svg
-Exec=/home/lazyhatter/Apps/$BITWARDEN
+Exec=/home/$USER/Apps/Bitwarden.AppImage
 Terminal=false
 MimeType=x-scheme-handler/bitwarden;text/html;
 EOF
 
-# Move entry
-mv Bitwarden.desktop $HOME/.local/share/applications/
+# Move entry to .desktop local applications folder
+mv ./Bitwarden.desktop $HOME/.local/share/applications/
+
+echo "---------- Bitwarden Installed ----------"
