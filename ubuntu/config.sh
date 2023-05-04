eadd28690157152d9ce6afb64eef614e98e81f02
dizpunk <102290380+hatpunk@users.noreply.github.com>
2023-05-04 22:50:13 +0200

    Add third-party selection to ubuntu config file

--- ubuntu/config.sh
+++ ubuntu/config.sh
@@ -16,3 +16,39 @@ gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
 printf "\n\n#################### Custom Bash Configurations\n\n"
 wget https://raw.githubusercontent.com/dizpunk/dotfiles/main/bash/bashrc &> /dev/null
 mv -f ./bashrc ~/.bashrc
+
+# Install chosen apps
+printf "\n\n#################### Third-party Apps\n\n"
+PS3="Select apps to install: "
+
+select apps in all choose none; do
+  case $apps in
+    all)
+      for script in ./apps/*.sh; do
+        bash "$script"
+      done
+      break
+      ;;
+    choose)
+      declare -A Apps
+      Apps=(["Bitwarden"]="bitwarden.sh" ["Brave Browser"]="bravebrowser.sh" ["VSCode"]="vscode.sh" ["Spotify"]="spotify.sh")
+      toInstall=()
+      for app in "${!Apps[@]}"; do
+        read -p "Do you want to install $app? (y/n) " answer
+        if [[ $answer == "y" ]]; then
+          toInstall+=("${Apps[$app]}")
+        fi
+      done
+      for script in "${toInstall[@]}"; do
+          bash "./apps/$script"
+      done
+      break
+      ;;
+    none)
+      break
+      ;;
+    *)
+      printf "Invalid option: $REPLY. Selection must be a number (1=all, 2=some, 3=none).\n\n"
+      ;;
+  esac
+done
