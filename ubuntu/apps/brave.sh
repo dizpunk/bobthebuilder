383d478622c27f29b0bdff23c8a27d1e379f2d88
dizpunk <102290380+dizpunk@users.noreply.github.com>
2023-10-15 18:56:13 +0200

    Add brave browser installation script for ubuntu

---
+++ ubuntu/apps/brave.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Install dependencies
+sudo apt install curl
+
+# Import Brave key
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+# Add Brave repository
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# Install Brave
+sudo apt update
+sudo apt install brave-browser
