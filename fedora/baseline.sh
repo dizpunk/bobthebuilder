84b8437f52f432ae102b382df6b39ef2fa098875
dizpunk <102290380+dizpunk@users.noreply.github.com>
2024-05-18 17:36:23 +0200

    Add terminal profiles to fedora baseline

--- fedora/baseline.sh
+++ fedora/baseline.sh
@@ -6,10 +6,13 @@ org.gnome.mutter workspaces-only-on-primary true
 gsettings set org.gnome.mutter center-new-windows true
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']" &> /dev/null
 
-
 # Switch firewalld default profile to drop
 sudo firewall-cmd --set-default-zone=drop
 
+# Set up terminal profiles
+wget https://raw.githubusercontent.com/dizpunk/dotfiles/main/gnome-terminal/gnome-terminal.dconf &> /dev/null
+dconf load /org/gnome/terminal/legacy/profiles:/ < ./gnome-terminal-profiles.dconf
+rm gnome-terminal-profiles.dconf
 
 # Update the system
 sudo dnf upgrade -y --refresh
